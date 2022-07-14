215a8e544b136e25d6626177b06771d57f38d5d8
Alex Loaiza Saldivar <alexloaiza.12.90@gmail.com>
2022-07-13 23:31:21 -0500

    Progress bar terminado

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  progreso: number = 50;
+  get getProgreso() {
+    return `${this.progreso}%`;
   }
+  cambiarValor(valor: number){
+    if (this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+    return this.progreso = this.progreso + valor;
+  }
+
 
 }
